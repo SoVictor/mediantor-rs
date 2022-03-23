dd66ae3d1d342a647aeea1d45316f4cda49b55ed
SoVictor <sovictorcom@yandex.ru>
2022-03-24 01:02:28 +0700

    Add mod mediantor

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,53 @@
 use std::io;
 
-trait Mediantor {
-	fn insert(&mut self, x: i32);
-	fn take(&mut self) -> i32;
-	fn size(&self) -> usize;
-}
+mod mediantor {
+	pub trait Mediantor {
+		fn insert(&mut self, x: i32);
+		fn take(&mut self) -> i32;
+		fn size(&self) -> usize;
+	}
 
-struct MediantorSortedVec {
-	elements: Vec<i32>
-}
+	pub struct MediantorSortedVec {
+		elements: Vec<i32>
+	}
 
-impl Mediantor for MediantorSortedVec {
-	fn insert(&mut self, x: i32) {
-		self.elements.push(x);
-		for i in (1..self.elements.len()).rev() {
-			if self.elements[i] < self.elements[i - 1] {
-				self.elements.swap(i, i - 1);
+	impl MediantorSortedVec {
+		pub fn new() -> Self {
+			Self{elements: Vec::new()}
+		}
+	}
+
+	impl Mediantor for MediantorSortedVec {
+		fn insert(&mut self, x: i32) {
+			self.elements.push(x);
+			for i in (1..self.elements.len()).rev() {
+				if self.elements[i] < self.elements[i - 1] {
+					self.elements.swap(i, i - 1);
+				}
 			}
 		}
-	}
 
-	fn take(&mut self) -> i32 {
-		let idx: usize = (self.elements.len() - 1) / 2;
-		let ans = self.elements[idx];
-		for i in idx..self.elements.len() - 1 {
-			self.elements.swap(i, i + 1);
+		fn take(&mut self) -> i32 {
+			let idx: usize = (self.elements.len() - 1) / 2;
+			let ans = self.elements[idx];
+			for i in idx..self.elements.len() - 1 {
+				self.elements.swap(i, i + 1);
+			}
+			self.elements.pop();
+			return ans;
 		}
-		self.elements.pop();
-		return ans;
-	}
-  
-	fn size(&self) -> usize {
-		return self.elements.len();
+	  
+		fn size(&self) -> usize {
+			return self.elements.len();
+		}
 	}
 }
 
 fn main() {
-	let mut mediantor = MediantorSortedVec{elements: Vec::new()};
+	use crate::mediantor::Mediantor;
+	use crate::mediantor::MediantorSortedVec;
+	
+	let mut mediantor = MediantorSortedVec::new();
 	
 	let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
