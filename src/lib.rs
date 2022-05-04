eb78aea562e763c1148008fb0019932ee79364be
SoVictor <sovictorcom@yandex.ru>
2022-05-05 00:47:00 +0300

    Implement Mediantor variant selection in example

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,17 @@ pub use crate::mediantor_sorted_vec::MediantorSortedVec;
 
 mod mediantor_sqrt_decomp;
 pub use crate::mediantor_sqrt_decomp::MediantorSqrtDecomp;
+
+pub enum MediantorImplementation {
+	MediantorHeap,
+	MediantorSqrtDecomp,
+	MediantorSortedVec,
+}
+
+pub fn create_mediantor(implementation: MediantorImplementation, max_size: usize) -> Box<dyn Mediantor> {
+	match implementation {
+		MediantorImplementation::MediantorHeap => Box::new(MediantorHeap::new()),
+		MediantorImplementation::MediantorSqrtDecomp => Box::new(MediantorSqrtDecomp::new(max_size)),
+		MediantorImplementation::MediantorSortedVec => Box::new(MediantorSortedVec::new()),
+	}
+}
